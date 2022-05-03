173ceeb99da844b5f5f31928cdd78570d744fd18
AngeloDalo <angelo251158@gmail.com>
2022-05-03 12:51:40 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        boolean luce = true; //o false
+        byte ilbyte = 5; //-128 a 127
+        short loshort = 1; //-32768 a 32767
+        int loint = 4; //-2 miliardi a 2 miliardi
+        long illong = 5; //-9quintilioni a 8 bytes
+        float ilfloat = 5.12f; //6-7 cifre dicimale
+        double ildouble = 5.12; //8 bytes
+        char lochar = 'c'; //singolo carattere
+        String lostring = "ciao"; //stringa
+        System.out.println(lostring.toUpperCase());
+    }
+}
